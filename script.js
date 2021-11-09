28dc67e3d8c9b6e618cea3ab4c5fd5e1ba6d59d7
Nasirul Islam <nasirul.islam@northsouth.edu>
2021-11-09 13:11:13 +0600

    add styling

---
+++ script.js
@@ -0,0 +1,47 @@
+const draggable_list = document.getElementById('draggable-list');
+const check = document.getElementById('check');
+
+const richestPeople = [
+    'Jeff Bezos',
+    'Bill Gates',
+    'Warren Buffett',
+    'Bernard Arnault',
+    'Carlos Slim Helu',
+    'Amancio Ortega',
+    'Larry Ellison',
+    'Mark Zuckerberg',
+    'Michael Bloomberg',
+    'Larry Page'
+];
+
+// Store ListItems
+const listItems = [];
+
+let dragStartIndex;
+
+createList();
+
+// const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// console.log(numbers.sort(function(a, b) { return a - b }));
+
+// Insert list items into Dom
+function createList() {
+    [...richestPeople]
+    .map(a => ({ value: a, sort: Math.random() }))
+    .sort((a, b) => a.sort - b.sort)
+    .forEach((person, index) => {
+        const listItem = document.createElement('li');
+        listItem.classList.add('list-item');
+        listItem.setAttribute('data-index', index);
+        listItem.innerHTML = `
+            <span class="number">${index + 1}</span>
+            <div class="draggable" draggable="true">
+                <p class="person-name">${person}</p>
+                <i class="fas fa-grip-lines"></i>
+            </div>
+        `;
+        listItems.push(listItem);
+        draggable_list.appendChild(listItem);
+    });
+}
+console.log(listItems);
